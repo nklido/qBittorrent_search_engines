ae881f81448d9abb2e609b0f404cd7fddcd0b9e4
Fernando F. H <100nandoo@users.noreply.github.com>
2024-06-11 15:32:51 +0800

    Update audiobookbay.py

--- engines/audiobookbay.py
+++ engines/audiobookbay.py
@@ -168,7 +168,7 @@ class audiobookbay(object):
 
 
     def request(self, searchTerm, category, page=1):
-        return retrieve_url(self.url+'?s='+searchTerm+'&cat='+category)
+        return retrieve_url(self.url+'?s='+searchTerm+'&cat='+category+'&tt=1')
 
     def search(self, what, cat='all'):
         category = self.supported_categories[cat]
